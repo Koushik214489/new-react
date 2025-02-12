162c085fb37ebdad74ea019b42fb72d199805116
Your Name <your-email@example.com>
2025-02-12 12:16:46 +0530

    React name and ph.no

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <h1>Koushikn Reddy KM </h1>
+    <h2>Phone number:- 9868287278</h2>
+    </>
+  )
+}
+
+export default App
